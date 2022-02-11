751229ab913ddbd5130bed93deda272120543122
Pervage Alam <pervage.alam@thoughtworks.com>
2022-02-11 23:51:47 +0530

    Updated the topic name

--- src/main/java/com/tw/pos/AppConfigs.java
+++ src/main/java/com/tw/pos/AppConfigs.java
@@ -4,5 +4,5 @@ public class AppConfigs {
     public final static String bootstrapServers = "localhost:9092";
     public final static String schemaRegistryServers = "http://localhost:8081";
     public final static String groupID = "PosValidatorGroup";
-    public final static String sourceTopicName = "pos";
+    public final static String sourceTopicName = "pos-simulator";
 }
